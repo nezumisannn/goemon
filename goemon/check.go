48621bc7d87617b95a82e25c5f9239fa103c6813
Yuki.Teraoka <teraoka@beyondjapan.com>
2019-04-17 23:34:18 +0900

    EC2インスタンスのイベント取得処理とチャットワークへのPOST処理をメソッド化

--- goemon/check.go
+++ goemon/check.go
@@ -27,11 +27,11 @@ type CheckFlag struct {
 
 // Config Unmarshal yaml file
 type Config struct {
-	Notifer []Notifer `mapstructure:"notifier"`
+	Notifier []Notifier `mapstructure:"notifier"`
 }
 
-// Notifer is a notification notifier
-type Notifer struct {
+// Notifier is a notification notifier
+type Notifier struct {
 	Notification string            `mapstructure:"notification"`
 	Region       string            `mapstructure:"region"`
 	Profile      string            `mapstructure:"profile"`
@@ -60,11 +60,9 @@ type RDSInfomation struct {
 // Unmarshal is store yaml in a structure
 func Unmarshal(file string) (err error) {
 	viper.SetConfigFile(file)
-
 	if err := viper.ReadInConfig(); err != nil {
 		return err
 	}
-
 	if err := viper.Unmarshal(&config); err != nil {
 		return err
 	}
@@ -74,7 +72,6 @@ func Unmarshal(file string) (err error) {
 // ConnectEC2 is Connect EC2 Service
 func ConnectEC2(session *session.Session, region string, profile string) (result *ec2.EC2) {
 	credential := credentials.NewSharedCredentials("", profile)
-
 	service := ec2.New(
 		session,
 		aws.NewConfig().WithRegion(region).WithCredentials(credential),
@@ -82,14 +79,13 @@ func ConnectEC2(session *session.Session, region string, profile string) (result
 	return service
 }
 
-// GetInstanceStatus is get list of instance status
-func GetInstanceStatus(service *ec2.EC2, instance string) (result *ec2.DescribeInstanceStatusOutput, err error) {
+// GetEC2InstanceStatus is get list of instance status
+func GetEC2InstanceStatus(service *ec2.EC2, instance string) (result *ec2.DescribeInstanceStatusOutput, err error) {
 	params := &ec2.DescribeInstanceStatusInput{
 		InstanceIds: []*string{
 			aws.String(instance),
 		},
 	}
-
 	response, err := service.DescribeInstanceStatus(params)
 	if err != nil {
 		return nil, err
@@ -97,46 +93,74 @@ func GetInstanceStatus(service *ec2.EC2, instance string) (result *ec2.DescribeI
 	return response, nil
 }
 
-// NotifyChatwork is Notify Chatwork
-func NotifyChatwork(chatwork []ChatworkNotifer, host string, code string, description string, notbefore string) {
+// GetEC2InstanceEvent is get event information from EC2 instance status
+func GetEC2InstanceEvent(notifier Notifier, ec2service *ec2.EC2) (results [][]string) {
+	var result [][]string
+	var event []string
+	for _, ec2 := range notifier.EC2 {
+		for _, instance := range ec2.Instances {
+			statuses, err := GetEC2InstanceStatus(ec2service, instance)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			for _, status := range statuses.InstanceStatuses {
+				for _, events := range status.Events {
+					event = append(event, instance)
+					event = append(event, *events.Code)
+					event = append(event, *events.Description)
+					event = append(event, events.NotBefore.Format(time.ANSIC))
+					result = append(result, event)
+				}
+			}
+		}
+	}
+	return result
+}
+
+// PostChatwork is post chatwork api
+func PostChatwork(roomid string, apikey string, body string) {
 	api := "https://api.chatwork.com/v2/rooms/"
 	path := "/messages"
-	completed := strings.Contains(description, "Completed")
+	url := api + roomid + path
 
-	if completed != true {
-		for _, notify := range chatwork {
-			roomid := notify.Roomid
-			apikey := notify.Apikey
-			url := api + roomid + path
+	request, err := http.NewRequest("POST", url, bytes.NewBufferString(body))
+	request.Header.Add("X-ChatWorkToken", apikey)
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-			body := "body="
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(response)
+}
 
-			for to := range notify.To {
-				body += "[To:" + strconv.Itoa(to) + "]"
+// NotifyChatwork is notify Chatwork message
+func NotifyChatwork(chatwork []ChatworkNotifer, ec2events [][]string) {
+	for _, ec2event := range ec2events {
+		completed := strings.Contains(ec2event[2], "Completed")
+		if completed != true {
+			for _, notify := range chatwork {
+				roomid := notify.Roomid
+				apikey := notify.Apikey
+				body := "body="
+
+				for to := range notify.To {
+					body += "[To:" + strconv.Itoa(to) + "]"
+				}
+
+				body += "\n"
+				body += "[info][title]Goemon AWS EC2 Schedule Event Notify[/title]"
+				body += "Host : " + ec2event[0] + "\n"
+				body += "Code : " + ec2event[1] + "\n"
+				body += "Description : " + ec2event[2] + "\n"
+				body += "NotBefore : " + ec2event[3] + " UTC [/info]"
+
+				PostChatwork(roomid, apikey, body)
 			}
-
-			body += "\n"
-			body += "[info][title]Goemon AWS EC2 Schedule Event Notify[/title]"
-			body += "Host : " + host + "\n"
-			body += "Code : " + code + "\n"
-			body += "Description : " + description + "\n"
-			body += "NotBefore : " + notbefore + " UTC [/info]"
-
-			request, err := http.NewRequest("POST", url, bytes.NewBufferString(body))
-
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			request.Header.Add("X-ChatWorkToken", apikey)
-			request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-			response, err := http.DefaultClient.Do(request)
-
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			fmt.Println(response)
 		}
 	}
 }
@@ -153,34 +177,18 @@ func Check(flag *CheckFlag) {
 		SharedConfigState: session.SharedConfigEnable,
 	}))
 
-	for _, notifier := range config.Notifer {
+	for _, notifier := range config.Notifier {
 		region := notifier.Region
 		profile := notifier.Profile
 		notification := notifier.Notification
 		chatwork := notifier.Chatwork
 
 		ec2service := ConnectEC2(session, region, profile)
-		for _, ec2 := range notifier.EC2 {
-			for _, instance := range ec2.Instances {
-				statuses, err := GetInstanceStatus(ec2service, instance)
-				if err != nil {
-					fmt.Println(err)
-					os.Exit(1)
-				}
+		ec2events := GetEC2InstanceEvent(notifier, ec2service)
 
-				for _, status := range statuses.InstanceStatuses {
-					for _, events := range status.Events {
-						code := *events.Code
-						description := *events.Description
-						notbefore := events.NotBefore.Format(time.ANSIC)
-
-						switch notification {
-						case "chatwork":
-							NotifyChatwork(chatwork, instance, code, description, notbefore)
-						}
-					}
-				}
-			}
+		switch notification {
+		case "chatwork":
+			NotifyChatwork(chatwork, ec2events)
 		}
 	}
 }
